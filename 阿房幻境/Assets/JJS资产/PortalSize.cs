bcf96c547db05e7298bb0515f38c1e132f4d57ef
jiangjinsheng-jjs <52699106+jiangjinsheng-jjs@users.noreply.github.com>
2020-11-20 22:12:04 +0800

    创建了测试场景2
    复现了C4D里面的基础着色和光照，添加了一个传送门放大缩小的脚本

---
+++ 阿房幻境/Assets/JJS资产/PortalSize.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalSize : MonoBehaviour
+{
+    public GameObject MyPlayer;
+    public GameObject RefMirror;
+        
+    public GameObject GroundEffect;
+
+   
+    Vector3 lastPosition;
+    Renderer rend;
+    float timer = 0;
+    bool SorM;
+
+    float SSize;
+    float RSize;
+    Vector3 GSize;
+
+    float Dlta;
+    // Start is called before the first frame update
+    void Start()
+    {
+        lastPosition = MyPlayer.transform.position = lastPosition;
+
+        rend = GetComponent<Renderer>();
+        //rend.material.shader = Shader.Find("Size");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SSize = rend.material.GetFloat("Size");
+        RSize = RefMirror.GetComponent<Renderer>().material.GetFloat("Size");
+        GSize = GroundEffect.transform.localScale;
+        if (lastPosition.x < MyPlayer.transform.position.x + 0.001f &&
+            lastPosition.x > MyPlayer.transform.position.x - 0.001f &&
+            lastPosition.z < MyPlayer.transform.position.z + 0.001f &&
+            lastPosition.z > MyPlayer.transform.position.z - 0.001f)
+        {
+            if (SorM)
+            {
+                timer = 0;
+            }
+
+            Debug.Log("S");
+            timer += Time.deltaTime;
+            
+            if (SSize < 0.7f)
+            {
+                SSize += timer/10;
+            }
+            
+            if (RSize < 0.7f)
+            {
+                RSize += timer / 10;
+            }
+            if (GSize.z < 0.3f)
+            {
+
+                GSize.z += timer / 10;
+            }
+            rend.material.SetFloat("Size", SSize);
+            RefMirror.GetComponent<Renderer>().material.SetFloat("Size", RSize);
+            GroundEffect.transform.localScale = GSize;
+            SorM = false;
+        }
+        else
+        {
+            if (!SorM)
+            {
+                timer = 0;
+            }
+            Debug.Log("M");
+            timer += Time.deltaTime;
+
+            if (SSize > 0.3f)
+            {
+                SSize -= timer/10;
+            }
+            if (RSize > 0.3f)
+            {
+                RSize -= timer / 10;
+            }
+            if (GSize.z > 0)
+            {
+
+                GSize.z -= timer / 10;
+            }
+            rend.material.SetFloat("Size", SSize);
+            RefMirror.GetComponent<Renderer>().material.SetFloat("Size", RSize);
+            GroundEffect.transform.localScale=GSize;
+            SorM = true;
+        }
+        
+        lastPosition = MyPlayer.transform.position;
+    }
+}
